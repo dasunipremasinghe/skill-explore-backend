00815a872b06b3646945d6815f62db383be3f06d
Dasuni Premasinghe <dasunimandini@gmail.com>
2025-03-22 18:52:09 +0530

    initialize skill-explorer-api project with Maven and Spring Boot setup

---
+++ src/main/java/lk/sliit/skillexplorer/api/SkillExplorerApiApplication.java
@@ -0,0 +1,13 @@
+package lk.sliit.skillexplorer.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillExplorerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillExplorerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/sliit/skillexplorer/api/SkillExplorerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.sliit.skillexplorer.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillExplorerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

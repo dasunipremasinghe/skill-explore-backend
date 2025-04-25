2449e2e38eb905f55061764c8535f50bc21fea38
N.P.Kaushani Dewmini <kaushani48952@gmail.com>
2025-04-25 20:15:56 +0530

    Created Comment and Comment Upload class

--- src/main/java/lk/sliit/skillexplorer/api/Entity/Comment.java
+++ src/main/java/lk/sliit/skillexplorer/api/Entity/Comment.java
@@ -1,4 +1,45 @@
 package lk.sliit.skillexplorer.api.Entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "comments")
 public class Comment {
+    @Id
+    private String id;
+    private String content;
+    private String postId;
+    private String userId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 }
